a6123c66c9bd1deb43a7a79f2965185c11fbb584
puzzleduck <PuZZleDucK@gmail.com>
2012-12-03 22:43:41 +1100

    add git ignore add main class

---
+++ tryGitJava/src/com/puzzleduck/trygit/TryGit.java
@@ -0,0 +1,22 @@
+/**
+ * (c) me & GPL3
+ */
+package com.puzzleduck.trygit;
+
+/**
+ * @author puzzleduck
+ *
+ */
+public class TryGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+		System.out.println("OK");
+
+	}
+
+}
